6ae5e99412e792210e99c49fe79004ebe83c63ac
Aymooonaaaa <mohamedayman0103@gmail.com>
2022-09-14 20:57:23 +0200

    NOT COMPLETED YET

--- Implement 4 tasks using EDF scheduler/main.c
+++ Implement 4 tasks using EDF scheduler/main.c
@@ -1,6 +1,6 @@
 /*
- * FreeRTOS V202112.00
- * Copyright (C) 2020 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+ * FreeRTOS Kernel V10.2.0
+ * Copyright (C) 2019 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -25,9 +25,9 @@
  * 1 tab == 4 spaces!
  */
 
-/* 
+/*
 	NOTE : Tasks run in system mode and the scheduler runs in Supervisor mode.
-	The processor MUST be in supervisor mode when vTaskStartScheduler is 
+	The processor MUST be in supervisor mode when vTaskStartScheduler is
 	called.  The demo applications included in the FreeRTOS.org download switch
 	to supervisor mode prior to main being called.  If you are not using one of
 	these demo application projects then ensure Supervisor mode is used.
@@ -37,81 +37,75 @@
 /*
  * Creates all the demo application tasks, then starts the scheduler.  The WEB
  * documentation provides more details of the demo application tasks.
- * 
- * Main.c also creates a task called "Check".  This only executes every three 
- * seconds but has the highest priority so is guaranteed to get processor time.  
+ *
+ * Main.c also creates a task called "Check".  This only executes every three
+ * seconds but has the highest priority so is guaranteed to get processor time.
  * Its main function is to check that all the other tasks are still operational.
- * Each task (other than the "flash" tasks) maintains a unique count that is 
- * incremented each time the task successfully completes its function.  Should 
- * any error occur within such a task the count is permanently halted.  The 
+ * Each task (other than the "flash" tasks) maintains a unique count that is
+ * incremented each time the task successfully completes its function.  Should
+ * any error occur within such a task the count is permanently halted.  The
  * check task inspects the count of each task to ensure it has changed since
- * the last time the check task executed.  If all the count variables have 
+ * the last time the check task executed.  If all the count variables have
  * changed all the tasks are still executing error free, and the check task
- * toggles the onboard LED.  Should any task contain an error at any time 
+ * toggles the onboard LED.  Should any task contain an error at any time
  * the LED toggle rate will change from 3 seconds to 500ms.
  *
  */
 
 /* Standard includes. */
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /* Scheduler includes. */
 #include "FreeRTOS.h"
 #include "task.h"
+#include "lpc21xx.h"
 
-/* Demo application includes. */
-#include "partest.h"
-#include "flash.h"
-#include "comtest2.h"
+/* Peripheral includes. */
 #include "serial.h"
-#include "PollQ.h"
-#include "BlockQ.h"
-#include "semtest.h"
-#include "dynamic.h"
+#include "GPIO.h"
+
+// More Application includes
+#include "queue.h"
+
+
 
 /*-----------------------------------------------------------*/
 
 /* Constants to setup I/O and processor. */
-#define mainTX_ENABLE		( ( unsigned long ) 0x00010000 )	/* UART1. */
-#define mainRX_ENABLE		( ( unsigned long ) 0x00040000 ) 	/* UART1. */
 #define mainBUS_CLK_FULL	( ( unsigned char ) 0x01 )
-#define mainLED_TO_OUTPUT	( ( unsigned long ) 0xff0000 )
 
 /* Constants for the ComTest demo application tasks. */
 #define mainCOM_TEST_BAUD_RATE	( ( unsigned long ) 115200 )
-#define mainCOM_TEST_LED		( 3 )
 
-/* Priorities for the demo application tasks. */
-#define mainLED_TASK_PRIORITY		( tskIDLE_PRIORITY + 2 )
-#define mainCOM_TEST_PRIORITY		( tskIDLE_PRIORITY + 2 )
-#define mainQUEUE_POLL_PRIORITY		( tskIDLE_PRIORITY + 2 )
-#define mainBLOCK_Q_PRIORITY		( tskIDLE_PRIORITY + 2 )
-#define mainSEM_TEST_PRIORITY		( tskIDLE_PRIORITY + 1 )
-#define mainCHECK_TASK_PRIORITY		( tskIDLE_PRIORITY + 3 )
+// Constant to specify max number of characters in a message
+#define MESSAGE_LEN 25
 
-/* Constants used by the "check" task.  As described at the head of this file
-the check task toggles an LED.  The rate at which the LED flashes is used to
-indicate whether an error has been detected or not.  If the LED toggles every
-3 seconds then no errors have been detected.  If the rate increases to 500ms
-then an error has been detected in at least one of the demo application tasks. */
-#define mainCHECK_LED				( 7 )
-#define mainNO_ERROR_FLASH_PERIOD	( ( TickType_t ) 3000 / portTICK_PERIOD_MS  )
-#define mainERROR_FLASH_PERIOD		( ( TickType_t ) 500 / portTICK_PERIOD_MS  )
+// Max Queue size
+#define MESSAGE_QUEUE_LEN 10
 
-/*-----------------------------------------------------------*/
 
-/*
- * Checks that all the demo application tasks are still executing without error
- * - as described at the top of the file.
- */
-static long prvCheckOtherTasksAreStillRunning( void );
+// Type definitions
+typedef struct
+{
+	//uint8_t sender_ID; // Number refers to the sender identity
+	char message[MESSAGE_LEN ]; // Message body to be sent
+	uint8_t message_len; // Actual length of the message
+} Message_st;
 
-/*
- * The task that executes at the highest priority and calls 
- * prvCheckOtherTasksAreStillRunning().  See the description at the top
- * of the file.
- */
-static void vErrorChecks( void *pvParameters );
+// Global Variables
+
+
+
+// Queue Handlers
+QueueHandle_t message_queue;
+
+
+// Task Handlers
+TaskHandle_t Button_1_Monitor_handler = NULL, Button_2_Monitor_handler = NULL;
+TaskHandle_t Periodic_Transmitter_handler = NULL, Uart_Receiver_handler = NULL;
+
 
 /*
  * Configure the processor for use with the Keil demo board.  This is very
@@ -119,37 +113,68 @@ static void vErrorChecks( void *pvParameters );
  * file.
  */
 static void prvSetupHardware( void );
-
 /*-----------------------------------------------------------*/
 
+// Prototypes for task codes
+void Button_1_Monitor(void *);
+void Button_2_Monitor(void *);
+void Periodic_Transmitter(void *);
+void Uart_Receiver(void *);
 
 
 /*
  * Application entry point:
- * Starts all the other tasks, then starts the scheduler. 
+ * Starts all the other tasks, then starts the scheduler.
  */
 int main( void )
 {
+	// Messages
+	Message_st B1_msg, B2_msg, string_message;
+
 	/* Setup the hardware for use with the Keil demo board. */
 	prvSetupHardware();
 
-	/* Start the demo/test application tasks. */
-	vAltStartComTestTasks( mainCOM_TEST_PRIORITY, mainCOM_TEST_BAUD_RATE, mainCOM_TEST_LED );
-	vStartLEDFlashTasks( mainLED_TASK_PRIORITY );
-	vStartPolledQueueTasks( mainQUEUE_POLL_PRIORITY );
-	vStartBlockingQueueTasks( mainBLOCK_Q_PRIORITY );
-	vStartSemaphoreTasks( mainSEM_TEST_PRIORITY );
-	vStartDynamicPriorityTasks();
+	// Create Message Queue
+	message_queue = xQueueCreate(MESSAGE_QUEUE_LEN, sizeof(Message_st *));
 
-	/* Start the check task - which is defined in this file.  This is the task
-	that periodically checks to see that all the other tasks are executing 
-	without error. */
-	xTaskCreate( vErrorChecks, "Check", configMINIMAL_STACK_SIZE, NULL, mainCHECK_TASK_PRIORITY, NULL );
+	/* Create Tasks here */
+	// Button 1 Task
+	xTaskCreate(Button_1_Monitor, // Function that implements the task.
+              "Button1 Task", // Text name for the task.
+              100, // Stack size in words, not bytes.
+              (void *) &B1_msg, // Parameter passed into the task.
+              1, // Priority at which the task is created.
+              &Button_1_Monitor_handler ); // Used to pass out the created task's handle.
+
+	// Button 2 Task
+	xTaskCreate(Button_2_Monitor, // Function that implements the task.
+              "Button2 Task", // Text name for the task.
+              100, // Stack size in words, not bytes.
+              (void *) &B2_msg, // Parameter passed into the task.
+              1, // Priority at which the task is created.
+              &Button_2_Monitor_handler ); // Used to pass out the created task's handle.
+
+	// Periodic String Task
+	xTaskCreate(Periodic_Transmitter, // Function that implements the task.
+              "String Task", // Text name for the task.
+              100, // Stack size in words, not bytes.
+              (void *) &string_message, // Parameter passed into the task.
+              1, // Priority at which the task is created.
+              &Periodic_Transmitter_handler ); // Used to pass out the created task's handle.
+
+	// Consumer Task
+	xTaskCreate(Uart_Receiver, // Function that implements the task.
+              "Consumer Task", // Text name for the task.
+              100, // Stack size in words, not bytes.
+              (void *) NULL, // Parameter passed into the task.
+              1, // Priority at which the task is created.
+              &Uart_Receiver_handler ); // Used to pass out the created task's handle.
+
 
 	/* Now all the tasks have been started - start the scheduler.
 
 	NOTE : Tasks run in system mode and the scheduler runs in Supervisor mode.
-	The processor MUST be in supervisor mode when vTaskStartScheduler is 
+	The processor MUST be in supervisor mode when vTaskStartScheduler is
 	called.  The demo applications included in the FreeRTOS.org download switch
 	to supervisor mode prior to main being called.  If you are not using one of
 	these demo application projects then ensure Supervisor mode is used here. */
@@ -161,90 +186,118 @@ int main( void )
 }
 /*-----------------------------------------------------------*/
 
-static void vErrorChecks( void *pvParameters )
-{
-TickType_t xDelayPeriod = mainNO_ERROR_FLASH_PERIOD;
-
-	/* Parameters are not used. */
-	( void ) pvParameters;
-
-	/* Cycle for ever, delaying then checking all the other tasks are still
-	operating without error.  If an error is detected then the delay period
-	is decreased from mainNO_ERROR_FLASH_PERIOD to mainERROR_FLASH_PERIOD so
-	the on board LED flash rate will increase.
-
-	This task runs at the highest priority. */
-
-	for( ;; )
-	{
-		/* The period of the delay depends on whether an error has been 
-		detected or not.  If an error has been detected then the period
-		is reduced to increase the LED flash rate. */
-		vTaskDelay( xDelayPeriod );
-
-		if( prvCheckOtherTasksAreStillRunning() != pdPASS )
-		{
-			/* An error has been detected in one of the tasks - flash faster. */
-			xDelayPeriod = mainERROR_FLASH_PERIOD;
-		}
-
-		/* Toggle the LED before going back to wait for the next cycle. */
-		vParTestToggleLED( mainCHECK_LED );
-	}
-}
-/*-----------------------------------------------------------*/
-
 static void prvSetupHardware( void )
 {
 	/* Perform the hardware setup required.  This is minimal as most of the
 	setup is managed by the settings in the project file. */
 
-	/* Configure the UART1 pins.  All other pins remain at their default of 0. */
-	PINSEL0 |= mainTX_ENABLE;
-	PINSEL0 |= mainRX_ENABLE;
+	/* Configure UART */
+	xSerialPortInitMinimal(mainCOM_TEST_BAUD_RATE);
 
-	/* LED pins need to be output. */
-	IODIR1 = mainLED_TO_OUTPUT;
+	/* Configure GPIO */
+	GPIO_init();
 
 	/* Setup the peripheral bus to be the same as the PLL output. */
 	VPBDIV = mainBUS_CLK_FULL;
 }
 /*-----------------------------------------------------------*/
 
-static long prvCheckOtherTasksAreStillRunning( void )
+void Button_1_Monitor(void *message)
 {
-long lReturn = pdPASS;
+	Message_st *B1_msg_ptr = (Message_st *)message;
 
-	/* Check all the demo tasks (other than the flash tasks) to ensure
-	that they are all still running, and that none of them have detected
-	an error. */
-	if( xAreComTestTasksStillRunning() != pdPASS )
+	pinState_t b1_old_state = GPIO_read(PORT_0, PIN0);
+	pinState_t b1_cur_state = b1_old_state;
+
+	while(1)
 	{
-		lReturn = pdFAIL;
-	}
+		b1_cur_state = GPIO_read(PORT_0, PIN0);
 
-	if( xArePollingQueuesStillRunning() != pdTRUE )
-	{
-		lReturn = pdFAIL;
-	}
+		if(b1_cur_state != b1_old_state)
+		{
+			if(b1_cur_state == PIN_IS_HIGH)
+			{
+				strcpy(B1_msg_ptr->message, " Rising Edge\n");
 
-	if( xAreBlockingQueuesStillRunning() != pdTRUE )
-	{
-		lReturn = pdFAIL;
-	}
+				B1_msg_ptr->message_len = (uint8_t) strlen(B1_msg_ptr->message);
+			}
+			else
+			{
+				strcpy(B1_msg_ptr->message, " Falling Edge\n");
 
-	if( xAreSemaphoreTasksStillRunning() != pdTRUE )
-	{
-		lReturn = pdFAIL;
-	}
+				B1_msg_ptr->message_len = (uint8_t) strlen(B1_msg_ptr->message);
+			}
 
-	if( xAreDynamicPriorityTasksStillRunning() != pdTRUE )
-	{
-		lReturn = pdFAIL;
-	}
+			xQueueSend(message_queue, (void *) &B1_msg_ptr, portMAX_DELAY);
+		}
 
-	return lReturn;
+		b1_old_state = b1_cur_state;
+
+		vTaskDelay(1);
+	}
 }
-/*-----------------------------------------------------------*/
 
+void Button_2_Monitor(void *message)
+{
+	Message_st *B2_msg_ptr = (Message_st *)message;
+
+	pinState_t b2_old_state = GPIO_read(PORT_0, PIN1);
+	pinState_t b2_cur_state = b2_old_state;
+
+	while(1)
+	{
+		b2_cur_state = GPIO_read(PORT_0, PIN1);
+
+		if(b2_cur_state != b2_old_state)
+		{
+			if(b2_cur_state == PIN_IS_HIGH)
+			{
+				strcpy(B2_msg_ptr->message, "Rising Edge\n");
+
+				B2_msg_ptr->message_len = strlen(B2_msg_ptr->message);
+			}
+			else
+				strcpy(B2_msg_ptr->message, " Falling Edge\n");
+
+				B2_msg_ptr->message_len = strlen(B2_msg_ptr->message);
+			}
+
+			xQueueSend(message_queue, (void *) &B2_msg_ptr, portMAX_DELAY);
+		}
+
+		b2_old_state = b2_cur_state;
+
+		vTaskDelay(1);
+	}
+
+
+void Periodic_Transmitter(void *message)
+{
+	Message_st *string_message_ptr = (Message_st *)message;
+
+	strcpy(string_message_ptr->message, " Message\n");
+
+	string_message_ptr->message_len = (uint8_t) strlen(string_message_ptr->message);
+
+	while(1)
+	{
+		xQueueSend(message_queue, (void *) &string_message_ptr, portMAX_DELAY);
+
+		vTaskDelay(100);
+	}
+}
+
+void Uart_Receiver(void *task_parameters)
+{
+	Message_st *msg_ptr;
+
+	while(1)
+	{
+		xQueueReceive(message_queue, (void *) &msg_ptr, portMAX_DELAY);
+
+		vSerialPutString((const signed char *)msg_ptr->message, msg_ptr->message_len);
+
+		vTaskDelay(50);
+	}
+}
 
